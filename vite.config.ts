c93321a5c67b9a2e4bacbd93ea25d0435879b2d3
Shpriz <shpriz1982@gmail.com>
2025-02-07 11:17:12 +0300

    Add some updates for dcoker.2

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['stomtest.nsmu.ru'],
+    host: true,
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://backend:3002',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
