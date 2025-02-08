f3b9714cf8c575b63f98c8fa5c372790528b3a4b
Shpriz <shpriz1982@gmail.com>
2025-02-08 20:19:53 +0300

    Configure for static IPs

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,6 @@ export default defineConfig(({ mode }) => ({
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom', 'react-bootstrap'],
-          // Другие зависимости, которые нужно разделить
         }
       }
     }
