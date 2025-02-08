4c5283d4d2518f05a652a18ccb0f2b652b59605d
Shpriz <shpriz1982@gmail.com>
2025-02-08 18:58:03 +0300

    Fix: Docker compose. Crypto

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,24 @@
-import './utils/uuid-patch';
+// Polyfill for crypto.randomUUID
+if (typeof crypto !== 'undefined' && !crypto.randomUUID) {
+  (crypto as any).randomUUID = function(): string {
+    const hexDigits = '0123456789abcdef';
+    let uuid = '';
+    
+    for (let i = 0; i < 36; i++) {
+      if (i === 8 || i === 13 || i === 18 || i === 23) {
+        uuid += '-';
+      } else if (i === 14) {
+        uuid += '4';
+      } else {
+        const r = Math.random() * 16 | 0;
+        uuid += hexDigits[(i === 19) ? (r & 0x3 | 0x8) : r];
+      }
+    }
+    
+    return uuid;
+  };
+}
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
